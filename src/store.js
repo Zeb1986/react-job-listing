a129880b1982c2c3b300e48922e2b89d2d7b182c
Viaches <viache86@gmail.com>
2022-08-27 22:00:43 +0300

    Redux persist added

--- src/store.js
+++ src/store.js
@@ -1,11 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { filterReducer } from "features/filter/filter-slice";
 import { positionReducer } from "features/positions/position-slice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from 'redux'
 
-export const store = configureStore({
-    reducer: {
+const persistConfig = {
+    key: 'root', 
+    storage
+}
+
+const rootReducer = combineReducers({
         filters: filterReducer,
         positions: positionReducer,
-    },
-    devTools: true,
 })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    devTools: true,
+});
+export const persistor = persistStore(store)
